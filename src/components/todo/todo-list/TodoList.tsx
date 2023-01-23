b9c94c1c08dc6f9306f5a3d5f5837cae5f89777d
DEE <92789566+Multimarix@users.noreply.github.com>
2023-01-23 18:49:19 +0100

    make tab navigation work(no functionality yet) (#1)
    Completed implementing Rendering of different lists on Tab navigation

--- src/components/todo/todo-list/TodoList.tsx
+++ src/components/todo/todo-list/TodoList.tsx
@@ -2,8 +2,11 @@ import { useEffect } from "react";
 import Footer from "./Footer";
 import ListItem from "./ListItem";
 import { useTaskStore } from "../../../stores/task-store";
+import ActiveList from "./lists/ActiveList";
+import CompletedList from "./lists/CompletedList";
 
 const TodoList = () => {
+  const activeTab = useTaskStore((state) => state.activeTab);
   const todos = useTaskStore((state) => state.todos);
   const setTodoStatus = useTaskStore((state) => state.setTodoStatus);
   const active = useTaskStore((state) => state.active);
@@ -12,25 +15,38 @@ const TodoList = () => {
     updateActive();
   }, []);
   console.log(todos);
+  // console.log(activeTab);
+
   return (
     <article className="bg-elem-light dark:bg-elem-dark-1 mt-5 mb-5 rounded-md shadow-2xl shadow-shadow-light dark:shadow-black">
-      <ul className="flex flex-col-reverse">
-        {todos.map(({ id, todo, complete }) => {
-          return (
-            <li
-              key={id}
-              className="border-b border-shadow-light dark:border-dark-txt-1"
-            >
-              <ListItem
-                id={id}
-                todo={todo}
-                complete={complete}
-                setTodoStatus={setTodoStatus}
-              />
-            </li>
-          );
-        })}
-      </ul>
+      {/* conditional rendering */}
+
+      {
+        <ul className="flex flex-col-reverse">
+          {activeTab === "active" ? (
+            <ActiveList setTodoStatus={setTodoStatus} todos={todos} />
+          ) : activeTab === "completed" ? (
+            <CompletedList todos={todos} setTodoStatus={setTodoStatus} />
+          ) : (
+            todos.map(({ id, todo, complete }) => {
+              return (
+                <li
+                  key={id}
+                  className="border-b border-shadow-light dark:border-dark-txt-1"
+                >
+                  <ListItem
+                    id={id}
+                    todo={todo}
+                    complete={complete}
+                    setTodoStatus={setTodoStatus}
+                  />
+                </li>
+              );
+            })
+          )}
+        </ul>
+      }
+
       {/* FOOTER (DISPLAY ITEMS INFO) */}
       <Footer active={active} />
     </article>
--- src/stores/task-store.ts
+++ src/stores/task-store.ts
@@ -6,13 +6,18 @@ import type { Actions, State, Todo, Input } from "../types/todo";
 export const useTaskStore = create(
   immer<State & Actions>((set, get) => ({
     todos: [],
+    allTodos: [],
+    activeTodos: [],
+    completedTodos: [],
+    activeTab: "all",
     active: 0,
     addTodo: (payload: Input): void => {
       set((state) => {
-        state.todos.push({
+        state.allTodos.push({
           id: crypto.randomUUID(),
           ...payload,
         });
+        state.todos = state.allTodos;
       });
       get().updateActive();
     },
@@ -32,17 +37,32 @@ export const useTaskStore = create(
         state.active = state.getActiveTodos().length;
       });
     },
+    getAllTodos: (): void => {
+      set((state) => {
+        state.todos = state.allTodos;
+        state.activeTab = "all";
+      });
+    },
     getActiveTodos: (): Todo[] => {
-      const activeTodos = get().todos.filter((todo) => {
+      const activeTodos = get().allTodos.filter((todo) => {
         return todo.complete === false;
+      });
+      set((state) => {
+        state.activeTodos = activeTodos;
+        state.todos = state.activeTodos;
+        state.activeTab = "active";
       });
       return activeTodos;
     },
-    getComletedTodos: (): Todo[] => {
-      const completeTodos = get().todos.filter((todo) => {
+    getComletedTodos: (): void => {
+      const completedTodos = get().allTodos.filter((todo) => {
         return todo.complete === true;
       });
-      return completeTodos;
+      set((state) => {
+        state.completedTodos = completedTodos;
+        state.todos = state.completedTodos;
+        state.activeTab = "completed";
+      });
     },
     deleteTodo: (id: string): void => {
       set((state) => {
